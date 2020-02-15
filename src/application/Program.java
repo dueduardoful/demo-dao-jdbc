2d61523857a1e2b4497ab31a9002cfdf9ce3f2fa
dueduardoful <dueduardoful@gmail.com>
2020-02-15 13:57:51 -0300

    Insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -31,7 +32,10 @@ public class Program {
 			System.out.println(obj);
 		}
 		
-		
+		System.out.println("=== TEST 4: seller Insert =====");
+		Seller newSeller = new Seller(null, "Greg", "Greg@gmail.com", new Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New id = " + newSeller.getId());
 	}
 
 }
